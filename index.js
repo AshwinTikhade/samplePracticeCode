8ebd9b7f64ded38a2f40576d60269f6a03c836bb
AshwinTikhade <atikhade2@gmail.com>
2024-12-16 23:52:43 +0530

    Missing Number in an Array.

---
+++ index.js
@@ -0,0 +1,20 @@
+let arr = [1, 2 , 5, 3]    // Find missing number in array
+
+
+function findMissingInArray() {
+    arr.sort((a,b) => (a-b))
+    console.log(arr)
+    let missingNum;
+    for(let i=0; i<arr.length; i++) {
+        
+        if(arr[i] !== i + 1) {
+            return i + 1
+        }
+        
+    }
+    return null
+}
+
+
+console.log("Missing Number:-", findMissingInArray())
+
